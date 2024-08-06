ead6a5fde469213d542a44a50467a75eb479f29b
Asif <49162687+nhAsif@users.noreply.github.com>
2024-08-06 10:09:54 +0600

    Alioth: add risingos flags

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -8,15 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution XYZ stuff.
-BUILD_WITH_GAPPS := true
+# Inherit some common RisingOS stuff.
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_ENABLE_BLUR := true
+RISING_MAINTAINER := nhAsif
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_BUILD_APERTURE_CAMERA := false
 TARGET_SUPPORTS_QUICK_TAP := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Build Type Flags
-EVO_BUILD_TYPE = DEVELOPMENT
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
